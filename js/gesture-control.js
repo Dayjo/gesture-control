9dfaa36217704713ad6ae3757f87d92a46f588da
Joel <joel@create.net>
2012-11-11 18:45:05 +0000

    make a little jquery plugin to add gesture control to an element

--- js/gesture-control.js
+++ js/gesture-control.js
@@ -2,16 +2,15 @@
  * Gesture control playground
  */
 
-var fingers = 0;
+//var fingers = 0;
 
- // *PING!* The DOM is ready
- $(function(){
+$.fn.extend({
+	gestures: function() { 
+		this.fingers = 0;
 
-
- 	$('body')
-	 	.on('touchstart', function(e) {
+		$(this).on('touchstart', function(e) {
 			
-	 		fingers = e.originalEvent.targetTouches.length;
+	 		this.touches = e.originalEvent.targetTouches;
 
 			//console.log('Start: ' + e.originalEvent.targetTouches.length);
 	 		
@@ -29,68 +28,70 @@ var fingers = 0;
 	 	.on('touchend', function(e) {
 	 		//console.log('End: ' + e.originalEvent.targetTouches.length);
 
+
 	 		if ( e.originalEvent.targetTouches.length == 0 ) {
 	 			
 	 			// Single Finger Tap
-	 			if ( fingers == 1 ) {
+	 			if ( this.touches.length == 1 ) {
 
 	 			}
 	 			// Double Finger Tap
-	 			else if ( fingers == 2 ) {
-
+	 			else if ( this.touches.length == 2 ) {
+	 				
+	 				//create_event("")
+					if (window.CustomEvent) {
+						var double_tap = document.createEvent('CustomEvent');
+						double_tap.initCustomEvent("tapTwoFingers", true, true, {touches: this.touches});
+						this.dispatchEvent(double_tap);
+					}
 	 			}
 	 			// Three Finger Tap
-	 			else if ( fingers == 3 ) {
+	 			else if ( this.touches.length == 3 ) {
 
 	 			}
-	 			$('#status').html(fingers);
-	 			// Reset to 0 as the touch has ended
-	 			touching = 0;
 
+	 			// Reset to 0 as the touch has ended
+	 			this.fingers = 0;
+	 		}
+	 	})
+	 	.on('mousedown', function(e) {
+	 		console.log(e.which);
+	 		// Double Tap
+	 		if ( e.which == 3 ) {
+
+				if (window.CustomEvent) {
+					var double_tap = document.createEvent('CustomEvent');
+					double_tap.initCustomEvent("tapTwoFingers", true, true, {touches: this.touches});
+					this.dispatchEvent(double_tap);
+				}
+ 		
 	 		}
 
-	 		/*$('#status').html('touchend');
+	 		if (e.preventDefault)
+			{
+				e.preventDefault();
+			}
+			else
+			{
+				e.returnValue = false;
+			}
+	 			
+	 	}).on('contextmenu', function(e){
 	 		e.preventDefault();
-	 		var i = 0;
-	 		$('.target').each(function(t,e) {
-	 			i++;
-	 			setTimeout(function(){$(e).fadeOut(function(){$(e).remove();});}, 400 + i * 10);
-	 		});
-	 		*/
-	 	})
-	 	.on('touchmove', function(e) {
 
-	 		console.log('Move: ' + e.originalEvent.targetTouches.length);
-	 		//moveTarget(e);
-	 		//$('#status').html('touchmove');
-	 		e.preventDefault();
-	 	})
-	 	.on('tap', function(fingers, e){
-	 		console.log(fingers);
-	 	});	
+	 	});
+
+
+	 	return this;
+	}
+});
+
+ // *PING!* The DOM is ready
+ $(function(){
+
+	$('body').gestures().on('tapTwoFingers', function(e) {
+		var touches = e.originalEvent.detail.touches;
+		console.log('Double Tap');
+	});
 
  });
-
- function moveTarget(e) {
-
- 	for ( i = 0; i < e.originalEvent.targetTouches.length; i++ ) {
-
- 		var x = e.originalEvent.targetTouches[i].pageX;
-	 	var y = e.originalEvent.targetTouches[i].pageY;
-	 		// offset for the middle of the object
-	 		x = x - $('#target').width()/2;
-	 		y = y - $('#target').height()/2;
-
-	 	var newtarget = document.createElement('div');
-	 		newtarget.className = 'target';
-	 		newtarget.style.left = x + 'px';
-	 		newtarget.style.top = y + 'px';
-
-	 	$('body').append(newtarget);
-
- 	}
-
- 	
-
- 	//$('#target').css({position: 'absolute', left: x, top: y});
- }
