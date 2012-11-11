216584c2b26a29e77ce17e78a10186344d6f15df
Joel <joel@create.net>
2012-11-11 15:59:51 +0000

    fiddling around with event object

--- js/gesture-control.js
+++ js/gesture-control.js
@@ -1,3 +1,36 @@
 /*
  * Gesture control playground
  */
+
+ // *PING!* The DOM is ready
+ $(function(){
+
+
+ 	$('body')
+	 	.on('touchstart', function(e) {
+	 		console.log('touchstart');
+	 		console.log(e.originalEvent.targetTouches[0].pageX + " " + e.originalEvent.targetTouches[0].pageY);
+	 		console.log('-----------');
+	 		$('#status').html('touchstart');
+
+	 		e.preventDefault();
+	 	})
+	 	.on('touchend', function(e) {
+	 		console.log('touchend');
+	 		console.log(e.originalEvent.targetTouches[0].pageX + " " + e.originalEvent.targetTouches[0].pageY);
+	 		console.log('-----------');
+	 		$('#status').html('touchend');
+
+	 		e.preventDefault();
+	 	})
+	 	.on('touchmove', function(e) {
+	 		console.log('touchmove');
+	 		console.log(e.originalEvent.targetTouches[0].pageX + " " + e.originalEvent.targetTouches[0].pageY);
+	 		console.log('-----------');
+	 		$('#status').html('touchstart');
+
+	 		e.preventDefault();
+	 	});
+	 	
+
+ });
