568ba5aebe893d9053fbc1ca7edfd5aa51abb872
Joel <joel@create.net>
2012-11-11 19:16:39 +0000

    Added a little bit of none-touch support for browsers. three fingers on a trackpad doesnt work.

--- js/gesture-control.js
+++ js/gesture-control.js
@@ -12,73 +12,113 @@ $.fn.extend({
 			
 	 		this.touches = e.originalEvent.targetTouches;
 
-			//console.log('Start: ' + e.originalEvent.targetTouches.length);
-	 		
-	 		// Ok so lets start creating some callbacks
-	 		if ( e.originalEvent.targetTouches ) {
-
-	 		}
-	 		/*this.middleclick(e);
-
-	 		moveTarget(e);
-	 		$('#status').html('touchstart');*/
 	 		e.preventDefault();
 	 		
 	 	})
 	 	.on('touchend', function(e) {
-	 		//console.log('End: ' + e.originalEvent.targetTouches.length);
-
 
 	 		if ( e.originalEvent.targetTouches.length == 0 ) {
 	 			
-	 			// Single Finger Tap
-	 			if ( this.touches.length == 1 ) {
 
-	 			}
-	 			// Double Finger Tap
-	 			else if ( this.touches.length == 2 ) {
-	 				
-	 				//create_event("")
-					if (window.CustomEvent) {
-						var double_tap = document.createEvent('CustomEvent');
-						double_tap.initCustomEvent("tapTwoFingers", true, true, {touches: this.touches});
-						this.dispatchEvent(double_tap);
+	 			
+	 			switch ( this.touches.length ) {
+	 			// One finger
+	 			case 1:
+	 				if (window.CustomEvent) {
+						tap = document.createEvent('CustomEvent');
+						tap.initCustomEvent("tapOneFinger", true, true, {touches: this.touches});
+						this.dispatchEvent(tap);
 					}
+					break;
+				// Two fingers
+				case 2: 
+					if (window.CustomEvent) {
+						tap = document.createEvent('CustomEvent');
+						tap.initCustomEvent("tapTwoFingers", true, true, {touches: this.touches});
+						this.dispatchEvent(tap);
+					}
+					break;
+				// Three Fingers	
+	 			case 3:
+					if (window.CustomEvent) {
+						tap = document.createEvent('CustomEvent');
+						tap.initCustomEvent("tapThreeFingers", true, true, {touches: this.touches});
+						this.dispatchEvent(tap);
+					}
+					break;
 	 			}
-	 			// Three Finger Tap
-	 			else if ( this.touches.length == 3 ) {
 
-	 			}
-
-	 			// Reset to 0 as the touch has ended
-	 			this.fingers = 0;
+				e.preventDefault();
 	 		}
 	 	})
 	 	.on('mousedown', function(e) {
-	 		console.log(e.which);
-	 		// Double Tap
-	 		if ( e.which == 3 ) {
+	 		var length = 0;
+	 		this.touches = {};
+	 		
+	 		this.touches['0'] = {
+ 									clientX: e.clientX,
+									clientY: e.clientY,
+									pageX: e.pageX,
+									pageY: e.pageY,
+									screenX: e.screenX,
+									screenY: e.screenY,
+									target: e.target,
+									__proto__: e.__proto__
+								};
 
-				if (window.CustomEvent) {
-					var double_tap = document.createEvent('CustomEvent');
-					double_tap.initCustomEvent("tapTwoFingers", true, true, {touches: this.touches});
-					this.dispatchEvent(double_tap);
-				}
- 		
+	 		switch ( e.which ) {
+	 			// One finger
+	 			case 1:
+	 				length = 1;
+	 				break;
+	 			case 2: 
+	 				length = 3;
+	 				this.touches['1'] = this.touches['3'] = this.touches['0'];
+ 					break;
+ 				case 3:
+ 					length = 2;
+ 					this.touches['1'] = this.touches['0'];
+
+ 					break;
 	 		}
 
-	 		if (e.preventDefault)
-			{
-				e.preventDefault();
-			}
-			else
-			{
-				e.returnValue = false;
-			}
+	 		this.touches['length'] = length;
+
+			e.preventDefault();
+	 	})
+	 	.on('mouseup', function(e) {
+	 		
+	 		switch ( e.which ) {
+	 			// One finger
+	 			case 1:
+	 				if (window.CustomEvent) {
+						tap = document.createEvent('CustomEvent');
+						tap.initCustomEvent("tapOneFinger", true, true, {touches: this.touches});
+						this.dispatchEvent(tap);
+					}
+					break;
+				// Three fingers
+				case 2: 
+					if (window.CustomEvent) {
+						tap = document.createEvent('CustomEvent');
+						tap.initCustomEvent("tapThreeFingers", true, true, {touches: this.touches});
+						this.dispatchEvent(tap);
+					}
+					break;
+				// Two Fingers	
+	 			case 3:
+					if (window.CustomEvent) {
+						tap = document.createEvent('CustomEvent');
+						tap.initCustomEvent("tapTwoFingers", true, true, {touches: this.touches});
+						this.dispatchEvent(tap);
+					}
+					break;
+	 		}
+
+			e.preventDefault();
 	 			
 	 	}).on('contextmenu', function(e){
 	 		e.preventDefault();
-
 	 	});
 
 
@@ -89,9 +129,17 @@ $.fn.extend({
  // *PING!* The DOM is ready
  $(function(){
 
-	$('body').gestures().on('tapTwoFingers', function(e) {
-		var touches = e.originalEvent.detail.touches;
-		console.log('Double Tap');
-	});
+	$('body')
+		.gestures()
+		.on('tapOneFinger', function(e) {
+			alert('One Finger!');
+		})
+		.on('tapTwoFingers', function(e) {
+			var touches = e.originalEvent.detail.touches;
+			alert('Two Fingers!');
+		})
+		.on('tapThreeFingers', function(e) {
+			alert('Three Fingers!');
+		});
 
  });
